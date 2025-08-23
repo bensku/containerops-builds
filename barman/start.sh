76933d64b3d05e254368af1301e41e4ac1760406
bensku <bmi@iki.fi>
2025-08-23 22:41:09 +0300

    Fixes to barman startup script

--- barman/start.sh
+++ barman/start.sh
@@ -6,12 +6,16 @@ for f in /etc/barman-sources/*.conf; do
     envsubst < $f > /etc/barman.d/$(basename $f)
 done
 
+chown barman:barman /var/lib/barman
+touch /var/log/barman.log
+chown barman:barman /var/log/barman.log
+
 # Sleep forever, but handle signals to exit cleanly
 # Why barman is run with podman exec is because container-ops
 # overlay networking is (too) closely coupled to container deployments
 pid=
 trap '[[ $pid ]] && kill $pid; exit' SIGINT
 trap '[[ $pid ]] && kill $pid; exit' SIGTERM
-sleep infinity & pid=$!
+tail -f /var/log/barman.log & pid=$!
 wait
 pid=
